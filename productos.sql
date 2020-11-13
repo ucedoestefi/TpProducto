b463aa541f4cee2c81b47f183bf2f6ead89f431d
ucedoestefi <ucedoestefi@hotmail.com>
2020-11-13 20:05:16 -0300

    Actualizacion 1

--- productos.sql
+++ productos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-10-2020 a las 22:23:41
+-- Tiempo de generación: 14-11-2020 a las 00:04:01
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.3.9
 
@@ -25,44 +25,104 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_prod` int(30) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `precio` int(255) NOT NULL,
+  `marca` varchar(100) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `subCategotia` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int(30) NOT NULL,
+  `tipo_rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id_rol`, `tipo_rol`) VALUES
+(1, 'Administrador'),
+(2, 'Empleado'),
+(3, 'Cliente');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `mail` varchar(50) NOT NULL,
-  `contrasenia` varchar(20) NOT NULL,
-  `perfil` varchar(30) NOT NULL
+  `password` varchar(20) NOT NULL,
+  `rol` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `mail`, `contrasenia`, `perfil`) VALUES
+INSERT INTO `usuarios` (`id_user`, `nombre`, `mail`, `password`, `rol`) VALUES
 (13, 'Senua', 'senua@gmail.com', 'locura', 'Empleado'),
-(14, 'Estefania ', 'ucedoestefi@gmail.com', 'Espinosa', 'Administrador');
+(14, 'Estefania ', 'ucedoestefi@gmail.com', 'Espinosa', 'Administrador'),
+(17, 'Dillion', 'dillion@gmail.com', 'Bulldog', 'empleado');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_prod`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_prod` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
